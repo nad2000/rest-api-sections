98ef3db550c60fee0368c8bed095b1f00395d7c8
Rad Cirskis <nad2000@gmail.com>
2017-02-12 02:28:03 +1300

    new store functions...

--- section3/app.py
+++ section3/app.py
@@ -6,6 +6,24 @@ app = Flask(__name__)
 def home():
     return "Hello, World!"
 
+@app.route("/store", methods=["POST"])
+def create_store():
+    pass
+
+@app.route("/store/<string:name>")
+def get_store(name):
+    pass
+
+
+@app.route("/store/<string:name>/item", methods=["POST"])
+def create_item_in_store(name):
+    pass
+
+
+@app.route("/store/<string:name>/item")
+def get_items_in_store(name):
+    pass
+
 if __name__ == "__main__":
     app.run(port=5000)
 
