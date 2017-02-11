31a56924e24859720a27f1df13db360aebe7752d
Rad Cirskis <nad2000@gmail.com>
2017-02-12 02:31:50 +1300

    added stores DB

--- section3/app.py
+++ section3/app.py
@@ -2,13 +2,23 @@ from flask import Flask
 
 app = Flask(__name__)
 
+stores = [
+        {
+            "name": "My Wonderful Store",
+            "items": [dict(name="My Item", price=15.99)]
+            }
+        ]
+
+
 @app.route('/')  # http://xxx/
 def home():
     return "Hello, World!"
 
+
 @app.route("/store", methods=["POST"])
 def create_store():
     pass
+
 
 @app.route("/store/<string:name>")
 def get_store(name):
@@ -23,6 +33,7 @@ def create_item_in_store(name):
 @app.route("/store/<string:name>/item")
 def get_items_in_store(name):
     pass
+
 
 if __name__ == "__main__":
     app.run(port=5000)
