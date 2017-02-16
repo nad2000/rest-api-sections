51f6a3efebc1f6e4adc124c91f4f57efbd5f940d
Rad Cirskis <nad2000@gmail.com>
2017-02-17 00:29:13 +1300

    items handler ...

--- section4/src/app.py
+++ section4/src/app.py
@@ -4,11 +4,29 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
-    def get(self, name):
-        return {"student": name}
 
-api.add_resource(Student, "/student/<string:name>")
+items = []
+
+class Item(Resource):
+
+    def get(self, name):
+        for i in items:
+            if i.get("name") == name:
+                return i
+        else:
+            return {"error": "Item '%s' not found" % name}, 404
+
+    def post(self, name):
+        item = {"name": name, "price": 12.99}
+        items.append(item)
+        return item, 201
+
+class ItemList(Resource):
+    def get(self):
+        return {"items": items}
+
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
